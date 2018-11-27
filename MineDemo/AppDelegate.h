3c3e773a3c238da0f0e113d819331f43743f7eaf
hoyifo <hoyifo@hoyifodemac-mini.local>
2018-11-27 13:42:34 +0800

    Initial Commit

---
+++ MineDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MineDemo
+//
+//  Created by hoyifo on 2018/11/27.
+//  Copyright © 2018 hoyifo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MineDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MineDemo
+//
+//  Created by hoyifo on 2018/11/27.
+//  Copyright © 2018 hoyifo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
